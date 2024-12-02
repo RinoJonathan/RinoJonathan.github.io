3739d5499aaa7b27050b7d0b93975ea9f48b527e
JustinFrost47 <rinojonathanarul@gmail.com>
2024-12-02 20:16:21 +0530

    relocated icons to assets folder and added email icon

---
+++ src/assets/EmailIcon.tsx
@@ -0,0 +1,8 @@
+
+export default function EmailIcon({style = "w-8 h-8" }) {
+  return (
+    <svg className={style} fill="#fafafa" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M22,5V9L12,13,2,9V5A1,1,0,0,1,3,4H21A1,1,0,0,1,22,5ZM2,11.154V19a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V11.154l-10,4Z"></path></g>
+    </svg>
+  )
+}
--- src/assets/MenuIcon.tsx
+++ src/assets/MenuIcon.tsx
@@ -1,8 +1,8 @@
 
-export default function MenuIcon() {
+export default function MenuIcon({style = "w-8 h-8" }) {
     return (
 
-            <svg className="w-12 h-12 mx-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={style} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
             <g id="SVGRepo_iconCarrier">
                 <path d="M4 6H20M4 12H20M4 18H20" stroke="#fafafa" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
