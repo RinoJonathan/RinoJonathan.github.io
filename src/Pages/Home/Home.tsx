867cc2f5da7401b84404559a9a2a3ba94e67565d
JustinFrost47 <rinojonathanarul@gmail.com>
2024-12-02 21:37:05 +0530

    implemented paralax effect and hollow arrow effect

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,20 @@
+import HeroSection from "./HeroSection";
+
+
+export default function Home() {
+  return (
+    <main className=" min-h-screen ">
+
+<HeroSection/>
+
+<section className=" z-20  min-h-screen w-full ">
+
+</section >
+<section className=" z-20  min-h-screen w-full bg-gradient-to-br from-tpurple to-tblue">
+  <div className="hollow-arrow">
+
+  </div>
+</section>
+    </main>
+  )
+}
