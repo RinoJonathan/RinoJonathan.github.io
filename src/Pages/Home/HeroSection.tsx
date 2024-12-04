467a3886d16aa43be2fec4eea1c85ca67258593f
JustinFrost47 <rinojonathanarul@gmail.com>
2024-12-05 03:00:32 +0530

    alright looking aroura overlay

--- src/Pages/Home/HeroSection.tsx
+++ src/Pages/Home/HeroSection.tsx
@@ -1,18 +1,17 @@
-import './Home.css'
+import './Home.css';
 
 export default function HeroSection() {
   return (
-    <section className=" hero hero-content min-h-screen  bg-gradient-to-br from-tpurple to-tblue flex flex-col justify-center items-start text-white">
-    <p className=" text-6xl p-8 px-28">Hi there!</p>
-    <p className="text-4xl p-8 px-28">I am Rino, I bring ideas into digital reality</p>
-    <p className="text-xl p-8 px-28">I am a computer science engineering student with a passion for building software . I believe in continuous learning and am eager to take on new challenges. Take a look at my work and feel free to reach out for a chat.</p>
-    <button className="text-xl mx-auto my-8 p-4 border rounded-md hover:bg-white hover:text-tblue">
-      Contact me
-    </button>
-    <div>
-      
-    </div>
-
-  </section>
-  )
+    <section className="hero hero-content min-h-screen bg-gradient-to-br from-tpurple to-tblue flex flex-col justify-center items-start text-white">
+      <div className="aurora"></div> {/* Aurora overlay */}
+      <p className="text-6xl p-8 px-28">Hi there!</p>
+      <p className="text-4xl p-8 px-28">I am Rino, I bring ideas into digital reality</p>
+      <p className="text-xl p-8 px-28">
+        I am a computer science engineering student with a passion for building software. I believe in continuous learning and am eager to take on new challenges. Take a look at my work and feel free to reach out for a chat.
+      </p>
+      <button className="text-xl mx-auto my-8 p-4 border rounded-md hover:bg-white hover:text-tblue">
+        Contact me
+      </button>
+    </section>
+  );
 }
